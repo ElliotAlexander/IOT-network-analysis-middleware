5952309ea598cba4d757a10345395b304362f360
Elliot Alexander <ep1e16@soton.ac.uk>
2019-11-14 08:53:42 +0000

    Removed unique not null constraint from nickname field

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS devices(
    uuid VARCHAR(128) PRIMARY KEY,
-   device_nickname VARCHAR(100) UNIQUE NOT NULL,
+   device_nickname VARCHAR(100),
    device_hostname VARCHAR(100),
    internal_ip_v4 VARCHAR(20),
    internal_ip_v6 VARCHAR(40),
