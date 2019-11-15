cf14927b3c488340d36b7aa9f7315754d5997658
Elliot Alexander <ep1e16@soton.ac.uk>
2019-11-15 13:20:26 +0000

    Added device stats table, fixed imports

---
+++ postgres-sql/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS devices(
+   uuid BYTEA PRIMARY KEY,
+   device_nickname VARCHAR(100),
+   device_hostname VARCHAR(100),
+   internal_ip_v4 VARCHAR(20),
+   internal_ip_v6 VARCHAR(40),
+   currently_active BOOLEAN NOT NULL,
+   last_seen TIMESTAMP,
+   first_seen TIMESTAMP NOT NULL,
+   set_ignored BOOLEAN NOT NULL,
+   device_type VARCHAR(100)
+);
+
+
+CREATE TABLE IF NOT EXISTS device_stats(
+   uuid BYTEA PRIMARY KEY,
+   packet_count BIGINT,
+   data_transferred BIGINT
+);
