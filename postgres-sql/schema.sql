b0bfe0d5155c61676d8ec691484a9103de062bda
Elliot Alexander <ep1e16@soton.ac.uk>
2020-01-10 20:19:42 +0000

    Added basic authentication setup, schema names need tidying up

--- postgres-sql/schema.sql
+++ postgres-sql/schema.sql
@@ -1,3 +1,10 @@
+DROP SCHEMA IF EXISTS example CASCADE;
+DROP SCHEMA IF EXISTS example_private CASCADE;
+CREATE SCHEMA example_private; 
+CREATE SCHEMA example;
+
+CREATE EXTENSION pgcrypto; 
+
 CREATE TABLE IF NOT EXISTS devices(
    uuid BYTEA PRIMARY KEY,
    mac_addr VARCHAR(64) NOT NULL,
@@ -46,3 +53,51 @@ CREATE TABLE IF NOT EXISTS device_dns_storage(
 );
 
 CREATE FUNCTION getDNSQueries(a bytea) RETURNS SETOF VARCHAR(100) AS $$ SELECT url FROM device_dns_storage WHERE uuid=a;$$ LANGUAGE SQL IMMUTABLE STRICT;
+
+CREATE TYPE example.jwt_token AS (
+  role TEXT,
+  exp INTEGER,
+  person_id INTEGER,
+  is_admin BOOLEAN,
+  username VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS example_private.person_account (
+  role TEXT,
+  exp INTEGER,
+  person_id INTEGER,
+  is_admin BOOLEAN,
+  username VARCHAR,
+  password_hash TEXT,
+  email TEXT
+);
+
+CREATE FUNCTION example.authenticate(
+  email text,
+  password text
+) RETURNS example.jwt_token AS $$
+DECLARE
+  account example_private.person_account;
+BEGIN
+  SELECT a.* INTO account
+    FROM example_private.person_account AS a
+    WHERE a.email = authenticate.email;
+
+  IF account.password_hash = crypt(password, account.password_hash) THEN
+    RETURN (
+      'person_role',
+      extract(epoch from now() + interval '7 days'),
+      account.person_id,
+      account.is_admin,
+      account.username
+    )::example.jwt_token;
+  ELSE
+    RETURN null;
+  END IF;
+END;
+$$ LANGUAGE plpgsql strict security definer;
+
+INSERT INTO example_private.person_account (email, password_hash) VALUES (
+  'test',
+  crypt('test',gen_salt('bf'))
+);
